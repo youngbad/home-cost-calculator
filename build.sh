20408914609695e0c934cf2946f592cdf8d5ff6f
youngbad <jakubbadowski.jb@gmail.com>
2026-03-09 21:19:40 +0100

    feat: Ensure database tables are created before running migrations

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Ensuring all database tables are created..."
+python -c "from app import create_app, db; app=create_app(); app.app_context().push(); db.create_all()"
+
 echo "Running migrations..."
 flask db upgrade
